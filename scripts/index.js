d2977bfef459f24aa256ca43a012861813650668
Gabriel Garcia <gabriel.garcia.ivan@outlook.com>
2022-05-12 23:53:51 -0300

    Segunda Version Interactuando con botones

--- scripts/index.js
+++ scripts/index.js
@@ -1,89 +1,73 @@
+const contenedor = document.getElementById("productoss");
+const tablaCarrito = document.getElementById("carritoLista");
+const carrito = [];
+
 const productos = [
-    {nombreProducto: "Razer DeathAdder V2", precio: 500, cantidad: 2},
-    {nombreProducto: "Onikuma CW905", precio: 1000, cantidad: 16},
-    {nombreProducto: "Redragon King Cobra", precio: 2000, cantidad: 8},
-    {nombreProducto: "Centrophorus v2", precio: 2500, cantidad: 23},
-    {nombreProducto: "Storm Elite M988", precio: 750, cantidad: 12},
-    {nombreProducto: "HP M260 Black", precio: 550, cantidad: 30},
+    {id:1, nombreProducto: "Razer DeathAdder V2", precio: 500, cantidad: 20, img:"img/Producto1.png"},
+    {id:2, nombreProducto: "Onikuma CW905", precio: 1000, cantidad: 16, img:"img/Producto2.png"},
+    {id:3, nombreProducto: "Redragon King Cobra", precio: 2000, cantidad: 18, img:"img/Producto3.png"},
+    {id:4, nombreProducto: "Centrophorus v2", precio: 2500, cantidad: 23, img:"img/Producto4.png"},
+    {id:5, nombreProducto: "Storm Elite M988", precio: 750, cantidad: 12, img:"img/Producto5.png"},
+    {id:6, nombreProducto: "HP M260 Black", precio: 550, cantidad: 30, img:"img/Producto6.png"},
+    {id:7, nombreProducto: "HP M160 RGB", precio: 3000, cantidad: 20, img:"img/Producto7.png"},
+    {id:8, nombreProducto: "Soul XM550", precio: 3250, cantidad: 18, img:"img/Producto8.png"},
+    {id:9, nombreProducto: "G903", precio: 5000, cantidad: 15, img:"img/Producto9.png"},
 ];
-productos.push({nombreProducto: "HP M160 RGB", precio: 3000, cantidad: 20})
-productos.push({nombreProducto: "Soul XM550", precio: 3250, cantidad: 18})
-productos.push({nombreProducto: "G903", precio: 5000, cantidad: 15})
 
-let contador = 0;
-let acumulador = 0;
-let respuesta = "si";
+const armarCard = (item) => {
+    return ( `
+        <article class="card productos">
+            <img src="${item.img}" class="card-img-top" alt="Imagen de un mouse ${item.nombreProducto}">
+            <div class="card-body">
+                <h2 class="productos__nombre">${item.nombreProducto}</h2>
+                <h3 class="productos__nombre">${item.precio}$</h3>
+                <h4 class="productos__nombre">Stock: ${item.cantidad}</h4>
+                <button onclick=agregarCarrito(${item.id}) class="productos_btn_color ${item.cantidad ? 'btn-primary' : 'btn-secondary'}" ${!item.cantidad ? 'disabled' : '' }><a class="">Agregar</a></button>
+            </div>
+        </article>`
+    );
+};
 
-for(let i = 0; i < productos.length ; i++){
-    console.log(`${i+1}_ Producto: ${productos[i].nombreProducto} Precio: ${productos[i].precio} Stock: ${productos[i].cantidad}`);
-}
+const armarCarrito = (item) => {
+    return( `
+    <tr>
+        <th scope="row">${item.id}</th>
+        <td>${item.nombreProducto}</td>
+        <td>${item.comprados}</td>
+        <td>$${item.precio * item.comprados} ($${item.precio})</td>
+    </tr>`
+    );
+};
 
-function calcularIva(producto){
-    let iva = producto * 0.21;
-    let precioFinal =  producto + iva;
-    return precioFinal;
-}
-
-function sumaCarrito(producto){
-    let a = producto -1;
-    acumulador = acumulador + calcularIva(productos[a].precio);
-    productos[a].cantidad--;
-}
-
-while(respuesta === "si"){
-
-    let producto = parseInt(prompt("Ingresa el numero del producto que deseas (1, 2 ,3...,9) -1 para filtrar por precio -2 para buscar por nombre")); 
-
-//Si no hay mas stock de un producto te da una nueva lista
-    productos.forEach(objetoProductos =>{
-        if(objetoProductos.cantidad === 0){
-
-            productos.splice(producto-1,producto);
-
-            for(let i = 0; i < productos.length ; i++){
-                console.log(`${i+1}. Producto: ${productos[i].nombreProducto} Precio: ${productos[i].precio} Stock: ${productos[i].cantidad}`);
-            }
-            alert("No hay mas stock");
-        }
+const cargarProductos = (datos, nodo, agregar) => {
+    let acumuladorDos = "";
+    datos.forEach((el) => {
+        acumuladorDos += agregar ? armarCarrito(el) : armarCard(el);
     })
+    nodo.innerHTML = acumuladorDos;
+};
 
-// Filtro de precios    
-    if(producto === -1){
-        let consulta = prompt("ingrese el precio maximo que quiere filtrar");
-        let resultado = productos.filter((el) => el.precio <= consulta)
-        resultado.forEach(objetoResultado =>{
-        console.log(`${objetoResultado.nombreProducto} ${objetoResultado.precio}`);
+const agregarCarrito = (id) => {
+    const seleccion = productos.find(item => item.id === id);
+    const busqueda = carrito.findIndex(el => el.id === id);
+
+    if(seleccion.cantidad === 0){
+        alert("no stock")
+    }
+
+    if (busqueda === -1) {
+        carrito.push({
+            id: seleccion.id,
+            nombreProducto: seleccion.nombreProducto,
+            precio: seleccion.precio,
+            comprados: 1,
         })
-        continue;
+        productos[0].cantidad--
+        
+    } else {
+        carrito[busqueda].comprados = carrito[busqueda].comprados + 1
     }
+    cargarProductos(carrito, tablaCarrito, true);
+};
 
-// Encontrar producto
-    if(producto === -2){
-        let consultaDos = prompt("ingrese el producto que quiere encontrar");
-        let resultadoDos = productos.find((elem) => elem.nombreProducto === consultaDos)
-        console.log(`Nombre: ${resultadoDos.nombreProducto} Precio: ${resultadoDos.precio} Stock disponible: ${resultadoDos.cantidad}`);
-        continue;
-    }
-    
-    if(producto > 9 || producto < -2 || producto === 0 ){
-        alert("dato erroneo");
-        break;
-    }else if(producto === 1,9){
-        sumaCarrito(producto);
-    }
-
-    contador++;
-
-// Te informa que productos vas agregando 
-    let carritoa = productos.slice(producto -1,producto);
-    console.log(`Agregó al carrito ${carritoa[0].nombreProducto}`);
-
-    respuesta = prompt("¿Desea agregar otro producto?");  
-}
-
-// Lista actualizada al finalizar
-for(let i = 0; i < productos.length ; i++){
-    console.log(`${i+1}- Producto: ${productos[i].nombreProducto} Precio: ${productos[i].precio} Stock: ${productos[i].cantidad}`);
-}
-
-alert(`La cantidad de produtos que agrego es ${contador} y el total de su compra es de ${acumulador}`);
+cargarProductos(productos, contenedor, false);
